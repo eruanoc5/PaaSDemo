545fd8ae1145c58abfda7331d60a2db699a547f2
edson.ruano <edsonruano1996@gmail.com>
2026-04-18 17:41:39 -0600

    Deployment automatico

--- obj/Debug/net10.0/AppAzureDemo.AssemblyInfo.cs
+++ obj/Debug/net10.0/AppAzureDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppAzureDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b771370252b607ffff868b22c57ed44415270160")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppAzureDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppAzureDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
